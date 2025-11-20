6e1a1bfd9b45d3679c10dd863bc409f29120dc58
TESSERACT1234 <93516931+TESSERACT1234@users.noreply.github.com>
2025-11-20 14:19:18 +0530

    Update firebase.js

--- client/client/src/firebase.js
+++ client/client/src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcdqscVdGtVV5BdHPJuiile7HTS29aGrE",
-    authDomain: "tesseract-flex-fuel-accounts.firebaseapp.com",
-    projectId: "tesseract-flex-fuel-accounts",
-    storageBucket: "tesseract-flex-fuel-accounts.firebasestorage.app",
-    messagingSenderId: "215365723420",
-    appId: "1:215365723420:web:a646845d2ae6bbaef086d9"
+  apiKey: "AIzaSyA8LHd8xS6gz5n0OF9qAojyR696tI41hxE",
+  authDomain: "new-accounts-7651d.firebaseapp.com",
+  projectId: "new-accounts-7651d",
+  storageBucket: "new-accounts-7651d.firebasestorage.app",
+  messagingSenderId: "428758227316",
+  appId: "1:428758227316:web:d4880f4b64725e30b07421",
+  measurementId: "G-6P36DLD1J9"
 };
 
 const app = initializeApp(firebaseConfig);
